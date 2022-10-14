91e7679f9e94e5c05f6b4fed4c275529811771f7
Francesco <francesco.voto.8@gmail.com>
2022-10-14 23:13:45 +0200

    chore: change eslint/prettier management

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  extends: ['@react-native-community', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+  },
+};
